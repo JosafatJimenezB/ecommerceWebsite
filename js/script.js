58c089721789348a98711f3a6273fc70085b2fab
JosafatJimenezB <josa221001@gmail.com>
2022-04-05 13:24:55 -0500

    add: agregando un loader

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function menutoggle(){
 }
 
 
+
+// agregando el loader
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+  function fadeOut(){
+    setInterval(loader, 3000);
+  }
+  
+  window.onload = fadeOut();
